d331b99dc18d7f3da752606b82af57002390ed5e
Renato Novaes <renatoalmeida49@gmail.com>
2021-07-16 16:52:19 -0300

    build(app.js, server.js): initial config of the server

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+
+app.use(morgan('dev'))
+app.use(express.urlencoded({extend: false}))
+app.use(express.json())
+app.use(cors())
+
+app.use((req, res, next) => {
+    return res.status(200).send({
+        mensagem: "A luta é grande, mas a derrota é certa"
+    })
+})
+
+module.exports = app
