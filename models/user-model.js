48b332630c66ca12828725b77f09308f55b5c211
Renato Novaes <renatoalmeida49@gmail.com>
2021-07-22 18:06:11 -0300

    correction of name bithdate to birthdate in user

--- models/user-model.js
+++ models/user-model.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, Sequelize) => {
         name: {
             type: Sequelize.STRING
         },
-        bithdate: {
+        birthdate: {
             type: Sequelize.DATE
         },
         city: {
