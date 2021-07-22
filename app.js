45e50ae068cf8cf849e5d71c7551650ebca62f9b
Renato Novaes <renatoalmeida49@gmail.com>
2021-07-22 18:06:36 -0300

    mode prod and dev for the database

--- app.js
+++ app.js
@@ -5,7 +5,14 @@ const morgan = require('morgan')
 const app = express()
 
 const db = require('./models/index')
+
+// MODE PROD
 db.sequelize.sync()
+
+// MODE DEV
+// db.sequelize.sync({ force: true }).then(() => {
+//     console.log("Drop and re-sync db.")
+// })
 
 app.use(morgan('dev'))
 app.use(express.urlencoded({extend: false}))
