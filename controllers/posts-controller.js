3919f45419d736f16ad4edc1856abf6abf021950
Renato Novaes <renatoalmeida49@gmail.com>
2021-07-23 10:13:08 -0300

    function to create post

---
+++ controllers/posts-controller.js
@@ -0,0 +1,30 @@
+const db = require('../models')
+
+const Post = db.posts
+
+exports.create = (req, res) => {
+    if (!req.body.title) {
+        return res.status(400).send({
+            message: "Content can not be empty!"
+        })
+    }
+
+    const post = {
+        id_user: req.user.id,
+        type: req.body.type,
+        body: req.body.body
+    }
+
+    Post.create(post)
+        .then(data => {
+            return res.status(201).send({
+                message: "Post added!",
+                data: data
+            })
+        })
+        .catch(err => {
+            return res.status(500).send({
+                message: err.message || "Erro while creating post"
+            })
+        })
+}
