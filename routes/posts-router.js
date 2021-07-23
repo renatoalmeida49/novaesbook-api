24fac062064e9b24f2bfae6461bbceabed631480
Renato Novaes <renatoalmeida49@gmail.com>
2021-07-23 10:38:45 -0300

    route to get home page of user

--- routes/posts-router.js
+++ routes/posts-router.js
@@ -5,6 +5,7 @@ const login = require('../middlewares/login')
 
 const PostsController = require('../controllers/posts-controller')
 
+router.post('/', login.login, PostsController.homePage)
 router.post('/new-post', login.login, PostsController.create)
 
 module.exports = router
