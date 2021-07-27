4c6c6db066e930fac168ed0c67016920f9e43b2a
Renato Novaes <renatoalmeida49@gmail.com>
2021-07-26 23:38:52 -0300

    feat(models/index.js): relations created

--- models/index.js
+++ models/index.js
@@ -15,4 +15,48 @@ db.userRelations = require('./user-relation-model')(sequelize, Sequelize)
 db.postLikes = require('./post-like-model')(sequelize, Sequelize)
 db.postComments = require('./post_comment-model')(sequelize, Sequelize)
 
+// ------- RELATIONS -------
+
+// USER has many POSTS 
+db.users.hasMany(db.posts, { as: "posts" })
+db.posts.belongsTo(db.users, {
+    foreignkey: "userId",
+    as: "user"
+})
+
+// USER has many COMMENTS in POSTS
+db.users.hasMany(db.postComments, { as: "comments" })
+db.postComments.belongsTo(db.users, {
+    foreignkey: "userId",
+    as: "user"
+})
+
+db.posts.hasMany(db.postComments, { as: "comments" })
+db.postComments.belongsTo(db.posts, {
+    foreignkey: "postId",
+    as: "post"
+})
+
+// USER has many LIKES in POSTS
+db.users.hasMany(db.postLikes, { as: "likes"})
+db.postLikes.belongsTo(db.users, {
+    foreignkey: "userId",
+    as: "user"
+})
+
+db.posts.hasMany(db.postLikes, { as: "likes" })
+db.postLikes.belongsTo(db.posts, {
+    foreignkey: "postId",
+    as: "post"
+})
+
+// USER has many FOLLOWERS
+db.userRelations.belongsTo(db.users, {
+    foreignkey: "userId",
+    as: "from"
+})
+db.userRelations.belongsTo(db.users, {
+    foreignkey: "userId",
+    as: "to"
+})
 module.exports = db
