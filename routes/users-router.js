e42ec08b981ca6f3a7636ff5afdb66e1247c3126
Renato Novaes <renatoalmeida49@gmail.com>
2021-07-29 16:32:23 -0300

    feat(users-router.js): route to get profile added

--- routes/users-router.js
+++ routes/users-router.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const router = express.Router()
 
+const login = require('../middlewares/login')
+
 const UsersController = require('../controllers/users-controller')
 
 router.post('/sign-up', UsersController.signUp)
 router.post('/sign-in', UsersController.signIn)
-router.put('/update', UsersController.update)
+router.put('/update', login.login, UsersController.update)
+router.post('/profile', login.login, UsersController.profile)
 
 module.exports = router
