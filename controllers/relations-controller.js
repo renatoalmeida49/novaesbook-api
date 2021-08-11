54522e61e8bf4c4dcc2ed88a1cb3ffabd03ef1f4
Renato Novaes <renatoalmeida49@gmail.com>
2021-08-11 08:21:20 -0300

    feat(relations-controller.js): follow and unfollow user

--- controllers/relations-controller.js
+++ controllers/relations-controller.js
@@ -2,23 +2,77 @@ const db = require('../models')
 
 const Relation = db.userRelations
 
-exports.relation = (req, res) => {
-    Relation.findOne({
-        where: {
-            fromId: req.body.user_from,
-            ToId: req.body.user_to
-        }
+function isFollowing(req, idUser) {
+    return new Promise((resolve, reject) => {
+        Relation.findOne({
+            where: {
+                fromId: req.user.id,
+                toId: idUser
+            }
+        })
+            .then(user => {
+                if (user) {
+                    return resolve(true)
+                }
+
+                return resolve(false)
+            })
     })
-        .then(user => {
-            return res.status(200).send({
-                message: "You got this",
-                user: user
-            })
+} 
+
+exports.relation = async (req, res) => {
+    const flag = await isFollowing(req, req.body.user_to)
+
+    return res.status(200).send({
+        message: "You got this",
+        flag: flag
+    })
+}
+
+exports.updateRelation = async (req, res) => {
+    const flag = await isFollowing(req, req.body.user_to)
+
+    if (flag) {
+        // Unfollow the user
+        Relation.findOne({
+            where: {
+                fromId: req.user.id,
+                toId: req.body.user_to
+            }
         })
-        .catch(err => {
-            return res.status(200).send({
-                message: "Erro",
-                erro: err
+            .then(result => {
+                result.destroy()
+
+                return res.status(201).send({
+                    message: "You have unfollowed it!"
+                })
             })
-        })
+            .catch(err => {
+                return res.status(500).send({
+                    message: "Something went wrong.",
+                    erro: err
+                })
+            })
+
+    } else {
+        // Follow the user
+        const relation = {
+            fromId: req.user.id,
+            toId: req.body.user_to
+        }
+
+        Relation.create(relation)
+            .then(data => {
+                return res.status(201).send({
+                    message: "You are following it!",
+                    relation: data
+                })
+            })
+            .catch(err => {
+                return res.status(500).send({
+                    message: "Something went wrong.",
+                    erro: err
+                })
+            })
+    }
 }
