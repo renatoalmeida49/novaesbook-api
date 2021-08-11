92b772683eed40e6b19da448b0c230f50257cc6d
Renato Novaes <renatoalmeida49@gmail.com>
2021-08-11 08:19:58 -0300

    feat(user-relations-router.js): route to follow or unfollow user

--- routes/user-relations-router.js
+++ routes/user-relations-router.js
@@ -6,5 +6,6 @@ const login = require('../middlewares/login')
 const RelationsController = require('../controllers/relations-controller')
 
 router.post('/', login.login, RelationsController.relation)
+router.put('/', login.login, RelationsController.updateRelation)
 
 module.exports = router
